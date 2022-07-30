47f7bf8da5cefc324a14082941d57836015ed117
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2022-07-30 17:52:42 -0300

    Done! ✔

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [topDistance, setTopDistance] = useState(0);
+
+  const handleScroll = () => {
+    setTopDistance(window.scrollY)
+  }
+
+  const Move = (speed) => topDistance * speed + "px"
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, [])
+
+  const parallax = {
+    nazuna: {transform: `translateY( ${Move(0.2)} )`},
+    logo: {transform: `translateY( -${Move(0.1)} )`},
+    star: {transform: `translate( -${Move(2)}, ${Move(0.1)} )`},
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="hero">
+        <img style={parallax.nazuna} className="nazuna" src="./Nazuna 1.png"/>
+        <img style={parallax.logo} className="logo" src="./Logo_White 1.png"/>
+        <img style={parallax.star} className="star" src="./Shooting_Star.png"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="content">
+        <blockquote>
+          <h1>SINOPSE</h1>
+          <p>Incapaz de dormir ou encontrar verdadeira satisfação em sua vida cotidiana, Ko Yamori para de ir à escola e começa a vagar pelas ruas à noite. Ele encontra uma garota chamada Nazuna Nanakusa, que é uma vampira e mostra a Ko as alegrias de ser um caminhante noturno. Isso resulta em Ko querendo ser um vampiro também, mas para alcançar seu objetivo ele tem que se apaixonar por ela primeiro.</p>
+        </blockquote>
+        <embed src="https://www.youtube.com/embed/a4bSbmqwhso?enablejsapi=1&wmode=opaque&autoplay=1"/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer>
+        <p>Feito com ❤ por <a href="https://github.com/Niwau" target="_blank">Niwau</a></p>
+      </footer>
     </div>
   )
-}
 
-export default App
+}
